1e4b51117ee58bd256bc75a49d1d330ca84eba7f
arinacoola <a.zvarunchyk@gmail.com>
2026-02-18 20:56:18 +0200

    біграми неперетинні

--- lab1/Frequencies.java
+++ lab1/Frequencies.java
@@ -14,9 +14,33 @@ public class Frequencies {
         return alphabet;
     }
 
-    public static int[][] countBigrams(String cleanText){
+    public static int[][] countBigramsIntersecting(String cleanText){
         int[][] bigrams = new int[32][32];
         for (int i = 0; i < cleanText.length() - 1; i++){
+            int ind1=0;
+            int ind2=0;
+            char c1 = cleanText.charAt(i);
+            char c2 = cleanText.charAt(i + 1);
+            if(c1==' '){
+                ind1 = 31;
+            }
+            else{
+                ind1=c1-'а';
+            }
+            if(c2==' '){
+                ind2=31;
+            }
+            else {
+                ind2=c2-'а';
+            }
+            bigrams[ind1][ind2]++;
+        }
+        return bigrams;
+    }
+
+    public static int[][] countBigramsNoIntersecting(String cleanText){
+        int[][] bigrams = new int[32][32];
+        for (int i = 0; i < cleanText.length() - 1; i += 2){
             int ind1=0;
             int ind2=0;
             char c1 = cleanText.charAt(i);
